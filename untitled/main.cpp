8da7d10de49433c5a9e799699ce6483fdd386204
imvickykumar <vickythetechguy@gmail.com>
2018-08-18 17:05:37 +0530

    strings

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -14,9 +14,33 @@ int main()
         scanf("%d",&arr[i]);
     }
 
-    for(i=n;i>0;i--){
-        printf("%d",&arr[i]);
+    for(i=n-1;i>=0;i--){
+        printf("%d",arr[i]);
     }
+
+
+    /*
+     * #include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+
+
+   scanf("%u",&a);
+   scanf("%u",&b);
+   c=a+b;
+   printf("The value c is %u\n", c);
+
+   _getch();
+   return 0;
+}
+Edit & Run
+
+     */
 }
 
 
---
+++ untitled1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+//    int arr[]={1,2,3};
+//
+//
+//    cout<<sizeof(arr)/sizeof(arr[0]
+
+//    string s;
+//    cin>>s;
+//    cout<<s;
+
+        string s="vicky";
+        string s1="umesh";
+
+        if(s1==s){
+            cout<<"wellen good";
+        }
+        else{
+            cout<<"haha";
+        }
+
+    return 0;
+}
